cf8805a825272c181f9915bac794f2899d46db68
Daniel Schlaug <daniel.schlaug@gmail.com>
2018-04-15 17:51:58 +0200

    Updated dependency versions

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject com.howardlewisship/bgg-graphql-proxy "0.0.1"
   :description "GraphQL interface to BoardGameGeek"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [io.aviso/logging "0.2.0"]
-                 [com.walmartlabs/lacinia "0.13.0"]
-                 [clj-http "2.3.0"]
+                 [com.walmartlabs/lacinia "0.25.0"]
+                 [clj-http "3.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.xml "0.0.8"]
-                 [io.pedestal/pedestal.service "0.5.2"]
-                 [io.pedestal/pedestal.jetty "0.5.2"]]
+                 [io.pedestal/pedestal.service "0.5.3"]
+                 [io.pedestal/pedestal.jetty "0.5.3"]]
   :codox {:source-uri "https://github.com/hlship/boardgamegeek-graphql-proxy/blob/master/{filepath}#L{line}"
           :metadata {:doc/format :markdown}})
